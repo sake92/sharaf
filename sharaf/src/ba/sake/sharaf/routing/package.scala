c2e8fd32b12598b0c757cc137024b8fb02d1f94d
Sakib Hadziavdic <sakib@sake.ba>
2023-10-26 11:26:08 +0200

    Add Routes.merge

--- sharaf/src/ba/sake/sharaf/routing/package.scala
+++ sharaf/src/ba/sake/sharaf/routing/package.scala
@@ -7,6 +7,13 @@ type RequestParams = (HttpString, Path)
 
 type Routes = Request ?=> PartialFunction[RequestParams, Response[?]]
 
+object Routes {
+  def merge(routess: Routes*): Routes =
+    routess.reduceLeft { case (acc, next) =>
+      acc.orElse(next)
+    }
+}
+
 object param {
   def unapply[T](str: String)(using fp: FromPathParam[T]): Option[T] =
     fp.parse(str)
