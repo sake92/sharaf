4844334f8ef07b8d224e22bdd64271412030bb68
sake92 <sakib@sake.ba>
2025-05-14 09:48:46 +0000

    Deploying to gh-pages from @ sake92/sharaf@247244843554deda8ed5ade9949314b4a56f96bf 🚀

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 
-// set anchorjs stuff
-var parent = "section";
 for (i = 1; i <= 6; i++) {
-    // CSS selectors "section h1", "section h2" ...
-    anchors.add(parent + ' h' + i);
+    // CSS selectors "h1", "h2" ...
+    anchors.add('h' + i);
 }
