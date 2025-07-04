dd539bc2601a20f498f0854176fca3d2bed74250
Sakib Hadziavdic <sakib@sake.ba>
2025-07-04 12:41:29 +0200

    Add snunit

---
+++ sharaf-snunit/src/ba/sake/sharaf/snunit/SharafRequestHandler.scala
@@ -0,0 +1,51 @@
+package ba.sake.sharaf.snunit
+
+import snunit.{Request as SnunitRequest, *}
+import ba.sake.sharaf.*
+import ba.sake.sharaf.routing.*
+import java.io.ByteArrayOutputStream
+
+class SharafRequestHandler(routes: Routes) extends RequestHandler {
+  override def handleRequest(snunitRequest: SnunitRequest): Unit = {
+    given Request = SnunitSharafRequest.create(snunitRequest)
+    val reqParams = fillReqParams(snunitRequest)
+    routes.definition.lift(reqParams) match {
+      case Some(res) =>
+        val headers = buildHeaders(res.headerUpdates)
+        res.body match {
+          case Some(body) =>
+            val aos = new ByteArrayOutputStream
+            res.rw.write(body, aos)
+            send(snunitRequest)(StatusCode(res.status.code), aos.toByteArray(), headers)
+          case None =>
+            send(snunitRequest)(StatusCode(res.status.code), "", headers)
+        }
+      case None =>
+        // will be catched by ExceptionHandler
+        throw exceptions.NotFoundException("route")
+    }
+  }
+
+  private def buildHeaders(hu: HeaderUpdates): Headers = {
+    val headerValues = hu.updates.flatMap {
+      case HeaderUpdate.Set(name, values) =>
+        Seq(name.value -> values.head)
+      case _ => Seq.empty
+    }
+    Headers(headerValues*)
+  }
+
+  private def fillReqParams(req: SnunitRequest): RequestParams = {
+    val method = HttpMethod.valueOf(req.method)
+    val originalPath = req.path
+    val relPath =
+      if originalPath.startsWith("/") then originalPath.drop(1)
+      else originalPath
+    val pathSegments = relPath.split("/")
+    val path =
+      if pathSegments.size == 1 && pathSegments.head == ""
+      then Path()
+      else Path(pathSegments*)
+    (method, path)
+  }
+}
---
+++ sharaf-snunit/src/ba/sake/sharaf/snunit/SnunitSharafRequest.scala
@@ -0,0 +1,44 @@
+package ba.sake.sharaf.snunit
+
+import java.nio.charset.StandardCharsets
+import scala.jdk.CollectionConverters.*
+import scala.jdk.StreamConverters.*
+import snunit.{Request as SnunitRequest, *}
+import ba.sake.formson.*
+import ba.sake.querson.*
+import ba.sake.sharaf.*
+import ba.sake.sharaf.exceptions.*
+
+class SnunitSharafRequest(underlyingRequest: SnunitRequest) extends Request {
+
+  /* *** HEADERS *** */
+  def headers: Map[HttpString, Seq[String]] =
+    val underlyingHeaders = underlyingRequest.headers
+    underlyingHeaders.toMap
+      .map { (headerName, headerValue) =>
+        HttpString(headerName) -> Seq(headerValue)
+      }
+
+  def cookies: Seq[Cookie] = ??? // TODO
+  // underlyingHttpServerExchange.requestCookies().asScala.map(CookieUtils.fromUndertow).toSeq
+
+  /* *** QUERY *** */
+  override lazy val queryParamsRaw: QueryStringMap =
+    underlyingRequest.query.split("&").flatMap( _.split("=") match {
+      case Array(key, value) => Seq(key -> Seq(value))
+      case _        => Seq.empty
+    })
+    .toMap
+
+  /* *** BODY *** */
+  override lazy val bodyString: String =
+    String(underlyingRequest.contentRaw(), StandardCharsets.UTF_8)
+
+  def bodyFormRaw: FormDataMap = ??? // TODO
+}
+
+object SnunitSharafRequest {
+
+  def create(underlyingRequest: SnunitRequest): SnunitSharafRequest =
+    SnunitSharafRequest(underlyingRequest)
+}
