1e0b4daa407251f4c111a1d818f85d15ce2f6ae7
Sakib Hadziavdic <sakib@sake.ba>
2026-01-05 17:30:18 +0100

    Remove unnecessary deps

--- examples/scala-cli/json_api.test.scala
+++ examples/scala-cli/json_api.test.scala
@@ -1,8 +1,9 @@
 //> using scala "3.7.0"
-//> using dep ba.sake::tupson:0.13.0
-//> using dep com.lihaoyi::requests:0.9.0
-//> using test.dep org.scalameta::munit::1.1.1
+//> using dep ba.sake::tupson:0.18.0
+//> using dep com.softwaremill.sttp.client4::core::4.0.13
+//> using test.dep org.scalameta::munit::1.2.1
 
+import sttp.client4.quick.*
 import ba.sake.tupson.*
 
 case class Car(brand: String, model: String, quantity: Int) derives JsonRW
@@ -13,23 +14,23 @@ class JsonApiSuite extends munit.FunSuite {
 
   test("create and get cars") {
     locally {
-      val res = requests.get(s"$baseUrl/cars")
-      val resBody = res.text.parseJson[Seq[Car]]
-      assertEquals(res.statusCode, 200)
+      val res = quickRequest.get(uri"${baseUrl}/cars").send()
+      val resBody = res.body.parseJson[Seq[Car]]
+      assertEquals(res.code.code, 200)
       assertEquals(res.headers("content-type"), Seq("application/json; charset=utf-8"))
-      assertEquals(res.text.parseJson[Seq[Car]], Seq.empty)
+      assertEquals(resBody, Seq.empty)
     }
 
     locally {
       val body = Car("Mercedes", "ML350", 1)
-      val res = requests.post(s"$baseUrl/cars", data = body.toJson)
-      assertEquals(res.statusCode, 200)
+      val res = quickRequest.post(uri"$baseUrl/cars").body(body.toJson).send()
+      assertEquals(res.code.code, 200)
     }
 
     locally {
-      val res = requests.get(s"$baseUrl/cars/Mercedes")
-      val resBody = res.text.parseJson[Seq[Car]]
-      assertEquals(res.statusCode, 200)
+      val res = quickRequest.get(uri"$baseUrl/cars/Mercedes").send()
+      val resBody = res.body.parseJson[Seq[Car]]
+      assertEquals(res.code.code, 200)
       assertEquals(res.headers("content-type"), Seq("application/json; charset=utf-8"))
       assertEquals(resBody, Seq(Car("Mercedes", "ML350", 1)))
     }
